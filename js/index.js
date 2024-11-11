2fc4498ecdb0d27c8437d3df5f6671cbde101cc5
Sami0189 <samisaimon28@gmail.com>
2024-11-12 01:08:09 +0600

    here i stop

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+//   document.getElementById("buyTicket").addEventListener("click", function () {
+//     const show = document.getElementById("show");
+//     show.style.display = "block";
+//     show.scrollIntoView({ behavior: "smooth", block: "start" });
+//     //show.scrollIntoView();
+//   });
+   
+
+
+//i can also do it by using onclick()
+function sami(){
+   const show = document.getElementById('show');
+   show.style.display='block';
+   show.scrollIntoView();
+}
